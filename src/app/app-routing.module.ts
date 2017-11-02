640221f3946b66713e4b52429f97d4d34c27b9a6
eason <crazy653555@gmail.com>
2017-11-02 08:54:22 +0800

    14 練習將獨立版型 /login 頁面加入並設定 LayoutComponent 乘載原本的所有頁面

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,22 @@ import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 import { fallbackRoute } from '../fallback-route';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { CardsComponent } from './cards/cards.component';
+import { LayoutComponent } from './layout/layout.component';
+import { LoginComponent } from './login/login.component';
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'cards/:type', component: CardsComponent },
   {
-    path: 'charts',
-    loadChildren: './charts/charts.module#ChartsModule'
+    path: '', component: LayoutComponent,
+    children: [
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'cards/:type', component: CardsComponent },
+      {
+        path: 'charts',
+        loadChildren: './charts/charts.module#ChartsModule'
+      }
+    ]
   },
+  {path:'login',component:LoginComponent},
   fallbackRoute
 ];
 
