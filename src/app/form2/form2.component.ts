ed86b88b37c04412e8008435a3ede8d52468641f
eason <crazy653555@gmail.com>
2017-11-09 23:38:08 +0800

    22 練習 Form Model 與 Data model 的關係，並練習 setValue, patchValue, reset 的用法

--- src/app/form2/form2.component.ts
+++ src/app/form2/form2.component.ts
@@ -1,50 +1,61 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormBuilder, Validators, FormArray } from "@angular/forms";
 
 @Component({
-  selector: 'app-form2',
-  templateUrl: './form2.component.html',
-  styleUrls: ['./form2.component.css']
+  selector: "app-form2",
+  templateUrl: "./form2.component.html",
+  styleUrls: ["./form2.component.css"]
 })
 export class Form2Component implements OnInit {
+  form: FormGroup;
 
-  form:FormGroup;
+  data = {
+    title: "Hello 2",
+    // subtitle: "World 2",
+    people: [
+      { name: "Will", tel: "0232423432", email: "will.huang@example.com" },
+      { name: "John", tel: "0934834734", email: "doggy@gmail.com" }
+    ]
+  };
 
-  constructor(private fb:FormBuilder) { }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit() {
     this.form = this.fb.group({
-      title:['Hello',Validators.required],
-      subtitle:['Avon',[
-        Validators.required,
-        Validators.minLength(2),
-        Validators.maxLength(20)
-      ]],
-      people:this.fb.array([
+      title: ["Hello", Validators.required],
+      subtitle: [
+        "Avon",
+        [Validators.required, Validators.minLength(2), Validators.maxLength(20)]
+      ],
+      people: this.fb.array([
         this.fb.group({
-          name:'',
-          tel:'',
-          email:''
+          name: "",
+          tel: "",
+          email: ""
         }),
         this.fb.group({
-          name:this.fb.control(''),
-          tel:'',
-          email:''
+          name: this.fb.control(""),
+          tel: "",
+          email: ""
         })
       ])
     });
+
+    this.form.patchValue(this.data);
   }
 
-  addPerson(){
-    console.log('addPerson()');
-    let people = this.form.get('people') as FormArray;
+  addPerson() {
+    console.log("addPerson()");
+    let people = this.form.get("people") as FormArray;
     people.push(
       this.fb.group({
-        name:'',
-        tel:'',
-        email:''
+        name: "",
+        tel: "",
+        email: ""
       })
-    )
+    );
   }
-
+  resetValue(){
+    this.form.reset(this.data);
+  }
 }
